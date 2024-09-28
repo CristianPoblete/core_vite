f10e1c85c6fed879c831bf7ada393fda5e04e30b
cristianpobleteds <cristianpobleteds@gmail.com>
2024-09-28 15:05:57 -0300

    sintento

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+const App = () => {
+
+
+  return (
+    <>
+      <main className="main">
+        <h1>Bienvenido a mi aplicacion React !</h1>
+
+        <div className="h2h3">
+          <h2>Lista de cosas por hacer </h2>
+
+          <h3>Aprender react</h3>
+          <h3>Practicar con vite</h3>
+          <h3>Construir proyectos increibles!</h3>
+
+        </div>
+
+      </main>
+    </>
+  )
+}
+
+export default App
